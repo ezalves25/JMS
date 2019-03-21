4acbbb86e0aed08e218505890e4fdaf2492d467b
Ezequias <ezalves25@gmail.com>
2019-03-20 22:31:02 -0300

    commit de um projeto para consumir mensagem por um fila

--- src/br/com/ezequias/jms/TesteConsumidor.java
+++ src/br/com/ezequias/jms/TesteConsumidor.java
@@ -1,23 +1,45 @@
 package br.com.ezequias.jms;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import org.apache.activemq.Message;
 import org.springframework.cglib.proxy.Factory;
 
 public class TesteConsumidor {
+	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 		
-		
+		//================================inicialização ===============================
 		InitialContext context=new InitialContext();
 		
-		ConnectionFactory factoy=(ConnectionFactory) context.lookup("ConnetionFactory");
+		ConnectionFactory factoy=(ConnectionFactory) context.lookup("ConnectionFactory");
 		Connection connection=factoy.createConnection();
+		connection.start();				
+		Session session=connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//============================================================================
 		
+		Destination fila=(Destination) context.lookup("financeiro");
+		MessageConsumer consumer= session.createConsumer(fila);
 		
+		Message message=(Message) consumer.receive();
+		
+		System.out.println("recebendo mensagem"+message);
+		
+		new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
 	}
 		
 }
